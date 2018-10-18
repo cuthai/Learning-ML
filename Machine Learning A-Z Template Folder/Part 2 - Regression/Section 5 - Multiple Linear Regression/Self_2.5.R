f4b87f2997c20d5fc487c421760c8d1c818706ec
Christopher Thai <christopheruthai@gmail.com>
2018-10-17 21:59:33 -0700

    Worked on 2.5

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/Self_2.5.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/Self_2.5.R
@@ -9,6 +9,7 @@ dataset$State = factor(dataset$State,
                        labels = c(1,2,3))
 
 # Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 0.8)
@@ -21,7 +22,9 @@ test_set = subset(dataset, split == FALSE)
 # test_set = scale(test_set)
 
 # Fitting Multiple Linear Regression to the Training set
-regressor = lm(formula = Profit ~ .,
+regressor = lm(formula = Profit ~ R.D.Spend,
                data = training_set)
+summary(regressor)
 
 # Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
