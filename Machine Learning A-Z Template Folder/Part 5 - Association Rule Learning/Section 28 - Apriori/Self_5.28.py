d8007ef3766a0773a5d9b0e1de0218054c4d48e2
Christopher Thai <christopheruthai@gmail.com>
2019-03-17 19:01:43 -0700

    Start 5.28

---
+++ Machine Learning A-Z Template Folder/Part 5 - Association Rule Learning/Section 28 - Apriori/Self_5.28.py
@@ -0,0 +1,16 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0,7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+    
+# Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions)
