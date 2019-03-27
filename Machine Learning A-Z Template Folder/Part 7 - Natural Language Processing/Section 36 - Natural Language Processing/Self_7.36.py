578bbe13e291011b05a05690f6b52c8960733ce7
Chris Thai <christopheruthai@gmail.com>
2019-03-27 14:50:50 -0700

    Did some more NLP

--- Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Self_7.36.py
+++ Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Self_7.36.py
@@ -9,3 +9,21 @@ import pandas as pd
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
+import re
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+
+operators = set(('and', 'or', 'not'))
+stop = set(stopwords.words('english')) - operators
+
+for i in range(0, len(dataset)):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in stop]
+    review = ' '.join(review)
+    corpus.append(review)
